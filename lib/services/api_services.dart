580a240dc16bde2caa8ea575d141e75878f4d421
anjumArnab <anjumarnab050@gmail.com>
2025-02-24 08:49:34 +0600

    search functionality added

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -65,3 +65,22 @@ Future<List<Recipe>?> getRecipe() async {
     return null;
   }
 }
+
+Future<List<Recipe>?> fetchRecipes(String query) async {
+  final url = Uri.parse('https://dummyjson.com/recipes/search?q=$query');
+
+  try {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      RecipeResponse recipeResponse = RecipeResponse.fromJson(data);
+      return recipeResponse.recipes;
+    } else {
+      throw Exception('Failed to load recipes');
+    }
+  } catch (e) {
+    print('Error fetching recipes: $e');
+    return null;
+  }
+}
